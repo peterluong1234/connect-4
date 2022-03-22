d5ba21c7fb477dc865f4b6bfe32982323b5118eb
Peter Luong <peterluong@Peters-MBP.socal.rr.com>
2022-03-22 15:03:58 -0700

    Added win conditions

--- js/app.js
+++ js/app.js
@@ -17,6 +17,8 @@ let column2Iterator = 0;
 let column3Iterator = 0;
 let column4Iterator = 0;
 let column5Iterator = 0;
+let column6Iterator = 0;
+let column7Iterator = 0;
 
 // assign div to array or is it the other way?
 // if clicked, change class/id to 'player1' or 'player2'
@@ -68,6 +70,8 @@ function render() {
         playerTurn = 1;
         playerValue = 2;
     }
+    checkVictory(playerChoices);
+
 }
 
 function handleClick(e) {
@@ -83,14 +87,16 @@ function handleClick(e) {
 
     // find out which column child have id
     // if not, add ID, if yes, move to next
-    if (e.target.id == '1') {
+    if (e.target.id == '1' && column1Iterator != 6) {
     // set id to player turn
-        while (column1Iterator < divEl.length) {
+        while (column1Iterator < 6) {
             if (divEl[column1Iterator].id != 'player1' || divEl[column1Iterator].id != 'player2') {
                 divEl[column1Iterator].id = currentTurn; 
                 playerChoices[0][column1Iterator] = playerValue;
                 column1Iterator++;
+                checkIteratorSize(column1Iterator,e.target);
                 console.log(playerChoices);
+                render();
             }
             break;
         }
@@ -103,6 +109,8 @@ function handleClick(e) {
                 divEl[column2Iterator].id = currentTurn; 
                 playerChoices[1][column2Iterator] = playerValue;
                 column2Iterator++;
+                checkIteratorSize(column2Iterator,e.target);
+                render();
             }
             break;
         }
@@ -115,6 +123,8 @@ function handleClick(e) {
                 divEl[column3Iterator].id = currentTurn; 
                 playerChoices[2][column3Iterator] = playerValue;
                 column3Iterator++;
+                checkIteratorSize(column3Iterator,e.target);
+                render();
             }
             break;
         }
@@ -127,6 +137,8 @@ function handleClick(e) {
                 divEl[column4Iterator].id = currentTurn; 
                 playerChoices[3][column4Iterator] = playerValue;
                 column4Iterator++;
+                checkIteratorSize(column4Iterator,e.target);
+                render();
             }
             break;
         }
@@ -139,6 +151,34 @@ function handleClick(e) {
                 divEl[column5Iterator].id = currentTurn; 
                 playerChoices[4][column5Iterator] = playerValue;
                 column5Iterator++;
+                checkIteratorSize(column5Iterator,e.target);
+                render();
+            }
+            break;
+        }
+    }
+    if (e.target.id == '6') {
+        // set id to player turn
+        while (column6Iterator < divEl.length) {
+            if (divEl[column6Iterator].id != 'player1' || divEl[column6Iterator].id != 'player2') {
+                divEl[column6Iterator].id = currentTurn; 
+                playerChoices[5][column6Iterator] = playerValue;
+                column6Iterator++;
+                checkIteratorSize(column6Iterator,e.target);
+                render();
+            }
+            break;
+        }
+    }
+    if (e.target.id == '7') {
+        // set id to player turn
+        while (column7Iterator < divEl.length) {
+            if (divEl[column7Iterator].id != 'player1' || divEl[column7Iterator].id != 'player2') {
+                divEl[column7Iterator].id = currentTurn; 
+                playerChoices[6][column7Iterator] = playerValue;
+                column7Iterator++;
+                checkIteratorSize(column7Iterator,e.target);
+                render();
             }
             break;
         }
@@ -155,10 +195,9 @@ function handleClick(e) {
     //         console.log(playerChoices);
     //     }
     //      break;
-        
     // }
 
-    render();
+
 }
 
 // NOTE TEST RETURN ITERATORS
@@ -193,9 +232,70 @@ function handleClick(e) {
 //         return;
 //     }
 // }
-function checkForVictory () {
-    // vertical victory
 
-}
 
 // set up function to return iterator
+// check for victory conditions: 
+
+function checkVictory (arr) {
+    // horizontal - WORKS
+    for(let i = 0; i < 7; i++) {
+      for(let j = 0; j < arr[0].length; j++) {
+        if (sumOfEle(arr[i][j], arr[i][j+1],arr[i][j+2],arr[i][j+3]) == 8) {
+          return victoryScreech();
+        } else if (sumOfEle(arr[i][j], arr[i][j+1],arr[i][j+2],arr[i][j+3]) == 20) {
+          return victoryScreech2();
+        } 
+      }
+    }
+  
+    // vertical - WORKS
+    for(let i = 0; i < 4; i++) {
+      for(let j = 0; j < 6; j++) {
+        if (sumOfEle(arr[i][j], arr[i+1][j],arr[i+2][j],arr[i+3][j]) == 8) {
+          return victoryScreech();
+        } else if (sumOfEle(arr[i][j], arr[i+1][j],arr[i+2][j],arr[i+3][j]) == 20) {
+          return victoryScreech2();
+        } 
+      }
+    }
+    // // diagonal - arrayview: bottom-left, top-right - WORKS
+      for(let i = 0; i < 4; i++) {
+        for(let j = 3; j < 6; j++) {
+          if (sumOfEle(arr[i][j], arr[i+1][j-1],arr[i+2][j-2],arr[i+3][j-3]) == 8) {
+            return victoryScreech();
+          } else if (sumOfEle(arr[i][j], arr[i+1][j-1],arr[i+2][j-2],arr[i+3][j-3]) == 20) {
+            return victoryScreech2();
+          } 
+        }
+      }
+    
+    // diagonal - arrayview: top-left, bottom-right
+      for(let i = 0; i < 4; i++) {
+        for(let j = 0; j < 3; j++) {
+          if (sumOfEle(arr[i][j], arr[i+1][j+1],arr[i+2][j+2],arr[i+3][j+3]) == 8) {
+            return victoryScreech();
+          } else if (sumOfEle(arr[i][j], arr[i+1][j+1],arr[i+2][j+2],arr[i+3][j+3]) == 20) {
+            return victoryScreech2();
+          } 
+        }
+      }
+  }
+  
+  function sumOfEle (a1, a2, a3, a4) {
+    return a1 + a2 + a3 + a4;
+  }
+  
+  function victoryScreech () {
+    console.log(`player 1 wins`)
+  }
+  
+  function victoryScreech2 () {
+    console.log(`player 2 wins!`)
+  }
+
+  function checkIteratorSize (it, e) {
+      if (it == 6) {
+        e.id = 'column-filled'
+      }
+  }
