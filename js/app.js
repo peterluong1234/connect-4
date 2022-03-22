babb3c1db005c742e10398c1a48a701251fa5947
Peter Luong <peterluong@Peters-MBP.socal.rr.com>
2022-03-22 15:18:51 -0700

    replaced e.target == '#' with function to reduce code size

--- js/app.js
+++ js/app.js
@@ -74,164 +74,198 @@ function render() {
 
 }
 
+
+
 function handleClick(e) {
     console.log(e.target.children);
     // console.log(e.target.children.getElementsByClassName('row').id);
 
     let divEl = e.target.getElementsByClassName('row');
 
- 
+    console.log(`${returnIterator(e.target)},${returnIterator2(e.target)}`);
     // return if column is not clicked
     if ( e.target.className != 'column') return;
 
 
     // find out which column child have id
     // if not, add ID, if yes, move to next
-    if (e.target.id == '1' && column1Iterator != 6) {
-    // set id to player turn
-        while (column1Iterator < 6) {
-            if (divEl[column1Iterator].id != 'player1' || divEl[column1Iterator].id != 'player2') {
-                divEl[column1Iterator].id = currentTurn; 
-                playerChoices[0][column1Iterator] = playerValue;
-                column1Iterator++;
-                checkIteratorSize(column1Iterator,e.target);
-                console.log(playerChoices);
-                render();
-            }
-            break;
-        }
-    }
+    // if (e.target.id == '1' && column1Iterator != 6) {
+    // // set id to player turn
+    //     while (column1Iterator < 6) {
+    //         if (divEl[column1Iterator].id != 'player1' || divEl[column1Iterator].id != 'player2') {
+    //             divEl[column1Iterator].id = currentTurn; 
+    //             playerChoices[0][column1Iterator] = playerValue;
+    //             column1Iterator++;
+    //             checkIteratorSize(column1Iterator,e.target);
+    //             console.log(playerChoices);
+    //             render();
+    //         }
+    //         break;
+    //     }
+    // }
     
-    if (e.target.id == '2') {
-        // set id to player turn
-        while (column2Iterator < divEl.length) {
-            if (divEl[column2Iterator].id != 'player1' || divEl[column2Iterator].id != 'player2') {
-                divEl[column2Iterator].id = currentTurn; 
-                playerChoices[1][column2Iterator] = playerValue;
-                column2Iterator++;
-                checkIteratorSize(column2Iterator,e.target);
-                render();
-            }
-            break;
-        }
-    }
+    // if (e.target.id == '2') {
+    //     // set id to player turn
+    //     while (column2Iterator < divEl.length) {
+    //         if (divEl[column2Iterator].id != 'player1' || divEl[column2Iterator].id != 'player2') {
+    //             divEl[column2Iterator].id = currentTurn; 
+    //             playerChoices[1][column2Iterator] = playerValue;
+    //             column2Iterator++;
+    //             checkIteratorSize(column2Iterator,e.target);
+    //             render();
+    //         }
+    //         break;
+    //     }
+    // }
 
-    if (e.target.id == '3') {
-        // set id to player turn
-        while (column3Iterator < divEl.length) {
-            if (divEl[column3Iterator].id != 'player1' || divEl[column3Iterator].id != 'player2') {
-                divEl[column3Iterator].id = currentTurn; 
-                playerChoices[2][column3Iterator] = playerValue;
-                column3Iterator++;
-                checkIteratorSize(column3Iterator,e.target);
-                render();
-            }
-            break;
-        }
-    }
+    // if (e.target.id == '3') {
+    //     // set id to player turn
+    //     while (column3Iterator < divEl.length) {
+    //         if (divEl[column3Iterator].id != 'player1' || divEl[column3Iterator].id != 'player2') {
+    //             divEl[column3Iterator].id = currentTurn; 
+    //             playerChoices[2][column3Iterator] = playerValue;
+    //             column3Iterator++;
+    //             checkIteratorSize(column3Iterator,e.target);
+    //             render();
+    //         }
+    //         break;
+    //     }
+    // }
 
-    if (e.target.id == '4') {
-        // set id to player turn
-        while (column4Iterator < divEl.length) {
-            if (divEl[column4Iterator].id != 'player1' || divEl[column4Iterator].id != 'player2') {
-                divEl[column4Iterator].id = currentTurn; 
-                playerChoices[3][column4Iterator] = playerValue;
-                column4Iterator++;
-                checkIteratorSize(column4Iterator,e.target);
-                render();
-            }
-            break;
-        }
-    }
+    // if (e.target.id == '4') {
+    //     // set id to player turn
+    //     while (column4Iterator < divEl.length) {
+    //         if (divEl[column4Iterator].id != 'player1' || divEl[column4Iterator].id != 'player2') {
+    //             divEl[column4Iterator].id = currentTurn; 
+    //             playerChoices[3][column4Iterator] = playerValue;
+    //             column4Iterator++;
+    //             checkIteratorSize(column4Iterator,e.target);
+    //             render();
+    //         }
+    //         break;
+    //     }
+    // }
 
-    if (e.target.id == '5') {
-        // set id to player turn
-        while (column5Iterator < divEl.length) {
-            if (divEl[column5Iterator].id != 'player1' || divEl[column5Iterator].id != 'player2') {
-                divEl[column5Iterator].id = currentTurn; 
-                playerChoices[4][column5Iterator] = playerValue;
-                column5Iterator++;
-                checkIteratorSize(column5Iterator,e.target);
-                render();
-            }
-            break;
-        }
-    }
-    if (e.target.id == '6') {
-        // set id to player turn
-        while (column6Iterator < divEl.length) {
-            if (divEl[column6Iterator].id != 'player1' || divEl[column6Iterator].id != 'player2') {
-                divEl[column6Iterator].id = currentTurn; 
-                playerChoices[5][column6Iterator] = playerValue;
-                column6Iterator++;
-                checkIteratorSize(column6Iterator,e.target);
-                render();
-            }
-            break;
-        }
-    }
-    if (e.target.id == '7') {
-        // set id to player turn
-        while (column7Iterator < divEl.length) {
-            if (divEl[column7Iterator].id != 'player1' || divEl[column7Iterator].id != 'player2') {
-                divEl[column7Iterator].id = currentTurn; 
-                playerChoices[6][column7Iterator] = playerValue;
-                column7Iterator++;
-                checkIteratorSize(column7Iterator,e.target);
-                render();
-            }
-            break;
-        }
-    }
+    // if (e.target.id == '5') {
+    //     // set id to player turn
+    //     while (column5Iterator < divEl.length) {
+    //         if (divEl[column5Iterator].id != 'player1' || divEl[column5Iterator].id != 'player2') {
+    //             divEl[column5Iterator].id = currentTurn; 
+    //             playerChoices[4][column5Iterator] = playerValue;
+    //             column5Iterator++;
+    //             checkIteratorSize(column5Iterator,e.target);
+    //             render();
+    //         }
+    //         break;
+    //     }
+    // }
+    // if (e.target.id == '6') {
+    //     // set id to player turn
+    //     while (column6Iterator < divEl.length) {
+    //         if (divEl[column6Iterator].id != 'player1' || divEl[column6Iterator].id != 'player2') {
+    //             divEl[column6Iterator].id = currentTurn; 
+    //             playerChoices[5][column6Iterator] = playerValue;
+    //             column6Iterator++;
+    //             checkIteratorSize(column6Iterator,e.target);
+    //             render();
+    //         }
+    //         break;
+    //     }
+    // }
+    // if (e.target.id == '7') {
+    //     // set id to player turn
+    //     while (column7Iterator < divEl.length) {
+    //         if (divEl[column7Iterator].id != 'player1' || divEl[column7Iterator].id != 'player2') {
+    //             divEl[column7Iterator].id = currentTurn; 
+    //             playerChoices[6][column7Iterator] = playerValue;
+    //             column7Iterator++;
+    //             checkIteratorSize(column7Iterator,e.target);
+    //             render();
+    //         }
+    //         break;
+    //     }
+    // }
 
+    console.log(e.target.className);
 
 
     // THIS CODE TO TEST WITH RETURN ITERATORS
-    // while (returnIterator(e.target) < divEl.length) {
-    //     if (divEl[returnIterator(e.target)].id != 'player1' || divEl[returnIterator(e.target)].id != 'player2') {
-    //         divEl[returnIterator(e.target)].id = currentTurn; 
-    //         playerChoices[returnIterator2(e.target)][returnIterator(e.target)] = playerValue;
-    //         returnIterator(e.target)++;
-    //         console.log(playerChoices);
-    //     }
-    //      break;
-    // }
-
-
-}
+    if (e.target.className == 'column'){
+        while (returnIterator(e.target) < divEl.length) {
+            if (divEl[returnIterator(e.target)].id != 'player1' || divEl[returnIterator(e.target)].id != 'player2') {
+                divEl[returnIterator(e.target)].id = currentTurn; 
+                playerChoices[returnIterator2(e.target)][returnIterator(e.target)] = playerValue;
+                // returnIterator(e.target) += 1; line not working with operator??? fix on line below
+                incrementIterator(e.target);
+                checkIteratorSize(returnIterator(e.target),e.target);
+                render();
+                console.log(playerChoices);
+            }
+            break;
+        }
+    }
+} // END OF HANDLECLICK
 
 // NOTE TEST RETURN ITERATORS
-// function returnIterator (elements){
-//     if (elements.id == '1') {
-//         return column1Iterator;
-//     } else if (elements.id == '2') {
-//         return column2Iterator;
-//     } else if (elements.id == '3') {
-//         return column3Iterator;
-//     } else if (elements.id == '4') {
-//         return column4Iterator;
-//     } else if (elements.id == '5') {
-//         return column5Iterator;
-//     } else {
-//         return;
-//     }
-// }
+function returnIterator (elements){
+    if (elements.id == '1') {
+        return column1Iterator;
+    } else if (elements.id == '2') {
+        return column2Iterator;
+    } else if (elements.id == '3') {
+        return column3Iterator;
+    } else if (elements.id == '4') {
+        return column4Iterator;
+    } else if (elements.id == '5') {
+        return column5Iterator;
+    } else if (elements.id == '6') {
+        return column6Iterator;
+    } else if (elements.id == '7') {
+        return column7Iterator;
+    } else {
+        return;
+    }
+}
 
-// function returnIterator2 (elements) {
-//     if (elements.id == '1') {
-//         return 0;
-//     } else if (elements.id == '2') {
-//         return 1;
-//     } else if (elements.id == '3') {
-//         return 2;
-//     } else if (elements.id == '4') {
-//         return 3;
-//     } else if (elements.id == '5') {
-//         return 4;
-//     } else {
-//         return;
-//     }
-// }
+function returnIterator2 (elements) {
+    if (elements.id == '1') {
+        return 0;
+    } else if (elements.id == '2') {
+        return 1;
+    } else if (elements.id == '3') {
+        return 2;
+    } else if (elements.id == '4') {
+        return 3;
+    } else if (elements.id == '5') {
+        return 4;
+    } else if (elements.id == '6') {
+        return 5;
+    } else if (elements.id == '7') {
+        return 6;
+    } else {
+        return;
+    }
+}
+
+function incrementIterator (elements){
+    if (elements.id == '1') {
+         column1Iterator++;
+    } else if (elements.id == '2') {
+         column2Iterator++;
+    } else if (elements.id == '3') {
+         column3Iterator++;
+    } else if (elements.id == '4') {
+        column4Iterator++;
+    } else if (elements.id == '5') {
+        column5Iterator++;
+    } else if (elements.id == '6') {
+        column6Iterator++;
+    } else if (elements.id == '7') {
+        column7Iterator++;
+    } else {
+        return;
+    }
+}
 
 
 // set up function to return iterator
